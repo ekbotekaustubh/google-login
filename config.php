3c2d1c761679f063db40af04295da4dfaffb1516
Kaustubh Ekbote <k.ekbote@easternenterprise.com>
2019-04-12 11:48:13 +0530

    Remove sensetive data

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ session_start();
 require_once "vendor/autoload.php";
 
 $gClient = new Google_Client();
-$gClient->setClientId('792923760460-h7703ob95h6rcl4gvasplf450o1i9dec.apps.googleusercontent.com');
-$gClient->setClientSecret('5XpJqvcLQJw5Mt783KSJqVmI');
+$gClient->setClientId('YOUR_CLIENT_ID');
+$gClient->setClientSecret('YOUR_CLIENT_SECRET');
 $gClient->setRedirectUri('http://localhost/gplus-quickstart-php/callback.php');
 $gClient->addScope('https://www.googleapis.com/auth/plus.login');
